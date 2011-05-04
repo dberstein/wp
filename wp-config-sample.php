061a2b4eb40d150adf240e3b41d5ef3b91fb523b
Daniel Berstein <danielbe@teleweb.co.il>
2011-05-04 12:38:28 +0300

    Allow by default MS.

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -79,12 +79,13 @@ define('WPLANG', '');
  * in their development environments.
  */
 define('WP_DEBUG', false);
+define ('WP_ALLOW_MULTISITE', true);
 
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
 if ( !defined('ABSPATH') )
-	define('ABSPATH', dirname(__FILE__) . '/');
+    define('ABSPATH', dirname(__FILE__) . '/');
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
